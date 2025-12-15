2ac5fd13e181d6f0a168e68f12a740bc85af23c6
Mark Milburn <mark.a.milburn@gmail.com>
2025-12-15 08:07:02 -0700

    Update autopairs & debug plugins
    Follow the modular layout in from:
    https://github.com/oriori1703/kickstart-modular.nvim

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -6,6 +6,8 @@
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
+---@module 'lazy'
+---@type LazySpec
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
