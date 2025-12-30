1ca7a2f253dab08f3070f594fe19d48a15ccec16
Mark Milburn <3363087+mmilburn@users.noreply.github.com>
2025-12-30 07:25:13 -0700

    Add incremental selection, treesitter-textobjects, additional parsers

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
     ---@type TSConfig
     ---@diagnostic disable-next-line: missing-fields
     opts = {
-      ensure_installed = { 'asm', 'bash', 'c', 'diff', 'dockerfile', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java', 'yaml' },
+      ensure_installed = { 'asm', 'bash', 'c', 'diff', 'dockerfile', 'go', 'html', 'java', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'rust', 'toml', 'vim', 'vimdoc', 'yaml' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -19,6 +19,16 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      -- Incremental selection based on syntax tree
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-s>',
+          node_incremental = '<C-s>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
@@ -32,6 +42,54 @@ return {
     opts = {
       max_lines = 3, -- How many lines of context to show
     },
+  },
+  { -- Treesitter textobjects for advanced text operations
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Jump to next/prev if not currently in a textobject
+            keymaps = {
+              -- Functions
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              -- Classes
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              -- Parameters/arguments
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- Add to jumplist
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              [']C'] = '@class.outer',
+              [']a'] = '@parameter.inner',
+            },
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              ['[C'] = '@class.outer',
+              ['[a'] = '@parameter.inner',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
+      }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
