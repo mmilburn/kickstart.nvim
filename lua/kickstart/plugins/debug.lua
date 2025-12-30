f0f7da1cbb37e7f5b5b473442b0a80366993b239
Mark Milburn <3363087+mmilburn@users.noreply.github.com>
2025-12-29 18:16:23 -0700

    Add java-debug-adapter and java-test to debug configuration

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -101,6 +101,8 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve', -- Go debugger
         'codelldb', -- C/C++/Rust/Assembly debugger
+        'java-debug-adapter',
+        'java-test',
       },
     }
 
