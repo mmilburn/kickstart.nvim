b01a74e47fd5128bb7172b68ec5779bdb4f2e78e
Mark Milburn <3363087+mmilburn@users.noreply.github.com>
2026-01-03 07:27:59 -0700

    Add render-markdown plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -59,6 +59,8 @@ require('lazy').setup({
 
   require 'kickstart.plugins.bufferline',
 
+  require 'kickstart.plugins.render-markdown',
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
