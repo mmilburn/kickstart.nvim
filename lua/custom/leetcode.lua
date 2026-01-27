edca02bf2d989de0b517f0fde5f732414fafb51b
Mark Milburn <3363087+mmilburn@users.noreply.github.com>
2026-01-27 05:09:14 -0700

    Add treesitter to leetcode dependencies

--- lua/custom/leetcode.lua
+++ lua/custom/leetcode.lua
@@ -5,6 +5,7 @@ return {
   build = ':TSUpdate html', -- if you have `nvim-treesitter` installed
   dependencies = {
     -- include a picker of your choice, see picker section for more details
+    'nvim-treesitter/nvim-treesitter',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
   },
